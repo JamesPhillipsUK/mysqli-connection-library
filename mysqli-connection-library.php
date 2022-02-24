a762ef64758b2044114c6febd23d094ab9b6be21
Jesse Phillips <JamesPhillipsUK@users.noreply.github.com>
2022-02-24 21:51:18 +0000

    Added type hints to parameters.  Added optional encoding to cleanString and CleanHtmlString.

--- mysqli-connection-library.php
+++ mysqli-connection-library.php
@@ -7,7 +7,7 @@ function connect()
 {
   $dBConnection = mysqli_connect(DBSERVER, DBUSER, DBPASS, DBNAME);
   if (!$dBConnection)
-    die("<p>BlogDraw couldn't connect to the database.</p>");
+    die("<p>Couldn't connect to the database.</p>");
   return $dBConnection;
 }
 
@@ -15,7 +15,7 @@ function connect()
  * Closes an open database connection.
  * @param dBConnection - The Database connection object.
  **/
-function disconnect($dBConnection)
+function disconnect(mysqli $dBConnection)
 {
   if (!empty($dBConnection))
     mysqli_close($dBConnection);
@@ -25,11 +25,12 @@ function disconnect($dBConnection)
  * Prevents xss/SQL injection
  * @param dBConnection - The database connection object currently in use.
  * @param string - The String to clean.
+ * @param encoding - (Optional) the encoding of the string (Defaults to UTF-8).
  * @return string - The string.
  **/
-function cleanString($dBConnection, $string)
+function cleanString(mysqli $dBConnection, string $string, string $encoding = "UTF-8")
 {
-  $string = mysqli_real_escape_string($dBConnection, mb_convert_encoding(htmlspecialchars($string), "UTF-8"));
+  $string = mysqli_real_escape_string($dBConnection, mb_convert_encoding(htmlspecialchars($string), $encoding));
   return $string;// Return the string.
 }
 
@@ -37,11 +38,12 @@ function cleanString($dBConnection, $string)
  * Prevents xss/SQL injection - doesn't strip HTML tags.
  * @param dBConnection - The database connection object currently in use.
  * @param string - The String to clean.
+ * @param encoding - (Optional) the encoding of the string (Defaults to UTF-8).
  * @return string - The string.
  **/
-function cleanHtmlString($dBConnection, $string)
+function cleanHtmlString(mysqli $dBConnection, string $string, string $encoding = "UTF-8")
 {
-  $string = mysqli_real_escape_string($dBConnection, mb_convert_encoding($string, "UTF-8"));
+  $string = mysqli_real_escape_string($dBConnection, mb_convert_encoding($string, $encoding));
   return $string;// Return the string.
 }
 ?>

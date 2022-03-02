a90c4a6f129cc3c290ab5d6d13bcadadb804619f
Jesse Phillips <JamesPhillipsUK@users.noreply.github.com>
2022-03-02 17:35:52 +0000

    Added phpDocumentor Docs.

---
+++ docs/js/searchIndex.js
@@ -0,0 +1,29 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\connect\u0028\u0029",
+            "name": "connect",
+            "summary": "Connects\u0020to\u0020a\u0020database.",
+            "url": "namespaces/default.html#function_connect"
+        },                {
+            "fqsen": "\\disconnect\u0028\u0029",
+            "name": "disconnect",
+            "summary": "Closes\u0020an\u0020open\u0020database\u0020connection.",
+            "url": "namespaces/default.html#function_disconnect"
+        },                {
+            "fqsen": "\\cleanString\u0028\u0029",
+            "name": "cleanString",
+            "summary": "Creates\u0020an\u0020\u0022escape\u0020string\u0022\u0020to\u0020help\u0020prevent\u0020SQL\u0020injection.",
+            "url": "namespaces/default.html#function_cleanString"
+        },                {
+            "fqsen": "\\cleanHtmlString\u0028\u0029",
+            "name": "cleanHtmlString",
+            "summary": "Creates\u0020an\u0020\u0022escape\u0020string\u0022\u0020to\u0020help\u0020prevent\u0020SQL\u0020injection\u0020\u002D\u0020doesn\u0027t\u0020strip\u0020HTML\u0020tags.",
+            "url": "namespaces/default.html#function_cleanHtmlString"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        }            ]
+);
